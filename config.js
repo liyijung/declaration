7bb43f4c01ef9f9e9644141571d669d9ee42fdcc
liyijung <132187351+liyijung@users.noreply.github.com>
2025-03-22 10:03:38 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-    API_URL: window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost"
-        ? "http://127.0.0.1:5000"
-        : "https://declaration-wi4s.onrender.com",
-    TOKEN_CHECK_INTERVAL: 43200000, // 12 小時 (毫秒)
-    PING_INTERVAL: 600000, // 10 分鐘 (毫秒)
-    ACTIVE_HOURS: { start: 8, end: 20 },
-};
