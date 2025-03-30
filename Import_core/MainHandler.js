0f540973e259f83318d0814b35741e3c503fe466
liyijung <132187351+liyijung@users.noreply.github.com>
2025-03-30 12:46:24 +0800

    Update MainHandler.js

--- Import_core/MainHandler.js
+++ Import_core/MainHandler.js
@@ -732,7 +732,7 @@ document.addEventListener("keydown", function(event) {
 function updateDocOtrDesc() {
     let copyDescMap = {
         'copy_2': '申請報單副本第二聯（進口證明用聯）',
-        'copy_3': '申請報單副本第三聯（沖退原料稅用聯）',
+        'copy_3': '申請報單副本第三聯（沖退原料稅用聯）\n附外銷品使用原料及其供應商資料清表',
         'copy_4': '申請報單副本第四聯（留底聯）',
         'copy_5': '申請報單副本第五聯（其他聯）'
     };
