89919c44655c5a7c21925ca18e27e778e905efe6
liyijung <132187351+liyijung@users.noreply.github.com>
2025-05-30 17:47:28 +0800

    Update companyDataSplit.py

--- companyData/companyDataSplit.py
+++ companyData/companyDataSplit.py
@@ -5,9 +5,9 @@ output_dir = r'C:\Users\Admin\Desktop\companyData'      # 輸出資料夾
 os.makedirs(output_dir, exist_ok=True)
 
 # 建立 0~9 的暫存檔
-files = {str(d): open(os.path.join(output_dir, f'companyData{d}.csv'), 'w', encoding='utf-8-sig') for d in range(10)}
+files = {str(d): open(os.path.join(output_dir, f'companyData{d}.csv'), 'w', encoding='utf-8') for d in range(10)}
 
-with open(input_file, 'r', encoding='utf-8-sig') as infile:
+with open(input_file, 'r', encoding='utf-8') as infile:
     header = infile.readline()
     for f in files.values():
         f.write(header)  # 每個檔案寫入表頭
